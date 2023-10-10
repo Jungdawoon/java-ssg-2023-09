c53dd916b0d34183c9d07f2670ece590de8bb8a0
Jungdawoon <Jungdawoon>
2023-10-10 21:26:40 +0900

    게시판 로그인기능구현까지

---
+++ src/com/jdw/java/ssg/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.jdw.java.ssg.controller;
+
+public abstract class Controller {
+	public abstract void doAction(String command, String actionMethodName);
+
+	
+}
---
+++ src/com/jdw/java/ssg/controller/MemberController.java
@@ -0,0 +1,147 @@
+package com.jdw.java.ssg.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.jdw.java.ssg.Util.Util;
+import com.jdw.java.ssg.dto.Member;
+
+/*
+
+ * 패키지 안 클래스를 클릭후 컨트롤+C/V로 복사붙여넣기 가능
+
+ * 
+
+ */
+
+public class MemberController extends Controller {
+	private List<Member> members;
+	private Scanner sc;
+	private String command;
+	private String actionMethodName;
+	private Member loginedMember;
+	
+	
+	public MemberController(Scanner sc) {
+		this.sc = sc;	
+		members = new ArrayList<Member>();
+	}
+	
+	public void doAction(String command, String actionMethodName) {
+		this.command = command;
+		this.actionMethodName = actionMethodName;
+		
+		switch (actionMethodName) {
+		case "join":
+			doJoin();
+			break;
+		case "login":
+			doLogin();
+			break;
+		default:
+			System.out.println("존재하지 않는 명령어 입니다.");
+		 	break;
+		}
+	}
+	private void doLogin() {
+		System.out.printf("로그인 아이디 : ");
+		String loginId = sc.nextLine();
+		System.out.printf("로그인 비번 : ");
+		String loginPw = sc.nextLine();
+
+		// 입력받은 아이디에 해당하는 회원이 존재하는지
+		Member member = getMemberByLoginId(loginId);
+
+		if ( member == null ) {
+			System.out.println("해당 회원은 존재하지 않습니다.");
+			return;
+		}
+
+		if (member.loginPw.equals(loginPw) == false) {
+			System.out.println("비밀번호를 확인해주세요.");
+			return;
+		}
+
+		loginedMember = member;
+		System.out.printf("로그인 성공! %s님 환영합니다^^!\n", loginedMember.name);
+	}
+
+	private void doJoin() {
+		int id = members.size() + 1;
+		String regDate = Util.getNowTimeStr();
+
+		String loginId = null;
+		String loginPw = null;
+		String loginPwConfirm = null;
+
+		while (true) {
+			System.out.printf("로그인 아이디 : ");
+			loginId = sc.nextLine();
+
+			if (isJoinableLoginId(loginId) == false) {
+				System.out.printf("%s(은)는 이미 사용중인 아이디입니다.\n", loginId);
+				continue;
+			}
+
+			break;
+		}
+
+		while (true) {
+			System.out.printf("로그인 비번 : ");
+			loginPw = sc.nextLine();
+			System.out.printf("로그인 비번확인 : ");
+			loginPwConfirm = sc.nextLine();
+
+			if (loginPw.equals(loginPwConfirm) == false) {
+				System.out.println("비밀번호를 다시 입력해주세요.");
+				continue;
+			}
+
+			break;
+		}
+
+		System.out.printf("이름 : ");
+		String name = sc.nextLine();
+
+		Member member = new Member(id, regDate, loginId, loginPw, name);
+		members.add(member);
+
+		System.out.printf("%d번 회원이 생성되었습니다. 환영합니다^^\n", id);
+	}
+	
+	private boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+
+			i++;
+		}
+
+		return -1;
+	}
+	private Member getMemberByLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1 ) {
+			return null;
+		}
+
+		return members.get(index);
+	}
+
+
+}
